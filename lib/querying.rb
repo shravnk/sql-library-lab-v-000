d9eafa93c61af5b6e6f7bf611f40abf7e994a926
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-13 20:48:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, count(species)
+  FROM characters
+  ORDER BY count(species) DESC LIMIT 1
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
