31b0da13abfb2520917d035ead6f317444eee6ba
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-13 21:09:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_name_and_series_subgenres_of_authors
   INNER JOIN series
   ON authors.id = series.author_id
   INNER JOIN subgenres
-  series.subgenre_id = subgenres.id
+  ON series.subgenre_id = subgenres.id
   "
 end
 
