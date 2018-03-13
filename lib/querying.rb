6838631a7b913a4c724f57db54325dd3055fc357
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-13 21:06:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,10 +22,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, subgenres.name
+  "SELECT authors.name, subgenres.name
   FROM authors
   INNER JOIN subgenres
-  ON authors.id = subgenres.id 
+  ON authors.id = subgenres.id
   "
 end
 
